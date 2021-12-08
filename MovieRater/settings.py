c05c66905330a6f688ab1f4913f9a7253c44af82
MehrezKanzari <kanzari.1993@gmail.com>
2021-12-08 22:01:41 +0100

    with firebase url

--- MovieRater/settings.py
+++ MovieRater/settings.py
@@ -139,5 +139,6 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "http://127.0.0.1:9000",
     "http://localhost:4200",
-    "movie-rater-app-front.firebaseapp.com"
+    "movie-rater-app-front.firebaseapp.com",
+    "http://127.0.0.1:8000/auth/"
 ]
